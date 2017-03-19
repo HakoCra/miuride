540892192456c38f9a7eae575c70768aa2969ff0
uehara <b1015062@fun.ac.jp>
2017-03-19 21:08:16 +0900

    str support

--- miuride_app/models.py
+++ miuride_app/models.py
@@ -6,7 +6,13 @@ class TourismPoint(models.Model):
     lat = models.DecimalField(max_digits=12, decimal_places=8)
     lng = models.DecimalField(max_digits=12, decimal_places=8)
 
+    def __str__(self):
+        return self.name
+
 
 class TourismCategory(models.Model):
     name = models.CharField('カテゴリ名', max_length=32)
     tourism_point = models.ForeignKey(TourismPoint, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
